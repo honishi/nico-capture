37d6498b7bd81bf84de4744981b6efa9658e4c3a
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2021-12-15 02:45:05 +0900

    add scraping logic

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 import configparser
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+LIVE_BASE_URL = "https://live.nicovideo.jp/watch/"
 
 
 class Community:
@@ -21,8 +27,7 @@ class NicoCapture:
         print(self.communities)
         for community in self.communities:
             print(community.community)
-            print(community.consumer_key)
-            print(community.token)
+            print(self.fetch_live_thumbnail(community.community))
 
     @staticmethod
     def read_communities():
@@ -40,6 +45,26 @@ class NicoCapture:
                 )
             )
         return communities
+
+    @staticmethod
+    def fetch_live_thumbnail(community):
+        response = requests.get(LIVE_BASE_URL + community)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        script_embedded_data = soup.select_one("script[id=\"embedded-data\"]")
+        if script_embedded_data is None:
+            return None
+        data_props = script_embedded_data["data-props"]
+        if data_props is None:
+            return None
+        data_props_json = json.loads(data_props)
+        program_status = data_props_json["program"]["status"]
+        print(program_status)
+
+        meta_image = soup.select_one("meta[property=\"og:image\"]")
+        if meta_image is None:
+            return None
+        return meta_image["content"]
 
 
 nico_capture = NicoCapture()
